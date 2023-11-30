20fe21618c92d0dcd9d76d6ee3b9813af99a80b1
Miguel Ángel Durán <miduga@gmail.com>
2023-11-30 19:50:31 +0100

    DEJA DE FUMARTE COSAS LOL

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,138 @@
-import Image from 'next/image'
+'use client'
+
+import { Button } from '@/components/ui/button'
+import { Form } from './form'
+import { useState } from 'react'
+import { DragAndDrop } from './draganddrop'
+
+const STEPS = {
+	INITIAL: 'INITIAL',
+	LOADING: 'LOADING',
+	PREVIEW: 'PREVIEW',
+	ERROR: 'ERROR',
+}
+
+const toBase64 = (file: File) => {
+	return new Promise<string>((resolve, reject) => {
+		const reader = new FileReader()
+		reader.readAsDataURL(file)
+		reader.onload = () => resolve(reader.result as string)
+		reader.onerror = (error) => reject(error)
+	})
+}
+
+async function* streamReader(res: Response) {
+	const reader = res.body?.getReader()
+	const decoder = new TextDecoder()
+	if (reader == null) return
+
+	while (true) {
+		const { done, value } = await reader.read()
+		const chunk = decoder.decode(value)
+		yield chunk
+		if (done) break
+	}
+}
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+	const [result, setResult] = useState('')
+	const [step, setStep] = useState(STEPS.INITIAL)
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+	const transformToCode = async (body: string) => {
+		setStep(STEPS.LOADING)
+		const res = await fetch('/api/generate-code-from-image', {
+			method: 'POST',
+			body,
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		})
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+		if (!res.ok || res.body == null) {
+			setStep(STEPS.ERROR)
+			throw new Error('Error al generar el código')
+		}
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+		setStep(STEPS.PREVIEW)
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+		// leer el streaming de datos
+		for await (const chunk of streamReader(res)) {
+			setResult((prev) => prev + chunk)
+		}
+	}
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+	const transformImageToCode = async (file: File) => {
+		const img = await toBase64(file)
+		await transformToCode(JSON.stringify({ img }))
+	}
+
+	const transformUrlToCode = async (url: string) => {
+		await transformToCode(JSON.stringify({ url }))
+	}
+
+	const [background, html = ''] = result.split('|||')
+
+	return (
+		<div className="grid grid-cols-[400px_1fr]">
+			<aside className="flex flex-col justify-between min-h-screen p-4 bg-gray-900">
+				<header className="text-center">
+					<h1 className="text-3xl font-semibold">Image 2 Code</h1>
+					<h2 className="text-sm opacity-75">Pasa tus imágenes a código en segundos</h2>
+				</header>
+
+				<section>{/* Aquí irán los filtros... */}</section>
+
+				<footer>Desarrollo por midudev</footer>
+			</aside>
+
+			<main className="bg-gray-950">
+				<section className="max-w-5xl w-full mx-auto p-10">
+					{step === STEPS.LOADING && (
+						<div className="flex justify-center items-center">
+							<div role="status">
+								<svg
+									aria-hidden="true"
+									className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+									viewBox="0 0 100 101"
+									fill="none"
+									xmlns="http://www.w3.org/2000/svg"
+								>
+									<path
+										d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+										fill="currentColor"
+									/>
+									<path
+										d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+										fill="currentFill"
+									/>
+								</svg>
+								<span className="sr-only">Loading...</span>
+							</div>
+						</div>
+					)}
+
+					{step === STEPS.INITIAL && (
+						<div className="flex flex-col gap-4">
+							<DragAndDrop transformImageToCode={transformImageToCode} />
+							<Form transformUrlToCode={transformUrlToCode} />
+						</div>
+					)}
+
+					{step === STEPS.PREVIEW && (
+						<div className="rounded flex flex-col gap-4">
+							<div
+								className="w-full h-full border-4 rounded border-gray-700 aspect-video"
+								style={{ backgroundColor: `#${background ? background.trim() : 'fff'}` }}
+							>
+								<iframe srcDoc={html} className="w-full h-full" />
+							</div>
+							<pre className="pt-10">
+								<code>{html}</code>
+							</pre>
+						</div>
+					)}
+				</section>
+			</main>
+		</div>
+	)
 }
